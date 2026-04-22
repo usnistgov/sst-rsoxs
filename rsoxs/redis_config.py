80f257db10bf2e685c02e1c0b472202854d1f9c5
Charles Titus <ctitus@bnl.gov>
2026-04-22 15:49:21 -0400

    Redis config needs to use new open_redis_client_from_settings function to get SSL. Though it should be rolled into configuration at some point

--- rsoxs/redis_config.py
+++ rsoxs/redis_config.py
@@ -1,12 +1,8 @@
 import redis  ## In-memory (RAM) databases that persists on disk even if Bluesky is restarted
 from redis_json_dict import RedisJSONDict
 from nbs_bl.beamline import GLOBAL_BEAMLINE as bl
-
+from nbs_bl.redisUtils import open_redis_client_from_settings
 
 redis_config_settings = bl.settings.get("redis").get("config", {})
-rsoxsredis = redis.Redis(
-    redis_config_settings.get("host", "info.sst.nsls2.bnl.gov"),
-    port=redis_config_settings.get("port", 60737),
-    db=redis_config_settings.get("db", 1),
-)
+rsoxsredis = open_redis_client_from_settings(redis_config_settings)
 rsoxs_config = RedisJSONDict(rsoxsredis, prefix=redis_config_settings.get("prefix", "rsoxs-"))
